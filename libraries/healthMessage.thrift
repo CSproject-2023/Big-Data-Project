0346ba1cd630075170e6d87fd24f0ab5a6c24751
Seif Wael <seifeldin7666@gmail.com>
2022-03-15 10:34:15 +0200

    Thrift file added

---
+++ libraries/healthMessage.thrift
@@ -0,0 +1,23 @@
+namespace java healthMessage
+namespace python healthMessage
+
+
+struct RamData{
+    1: double Total,
+    2: double Free,
+}
+struct DiskData{
+    1: double Total,
+    2: double Free,
+}
+struct Message{
+    1: string serviceName,
+    2: string Timestamp,
+    3: double CPU,
+    4: RamData RAM,
+    5: DiskData Disk,
+}
+
+service HealthMessage{
+    oneway void sendHealthMessage(1: Message message);
+}
